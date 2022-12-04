ab3358a04d25acd23339b804fbbcdea52b3a21b2
Fırat Alçın <firatalcin04@gmail.com>
2022-12-04 15:25:41 +0300

    Repository generic interface yazıldı ve entity özelinde referans alındı

---
+++ BlogProject/BlogProjectApp.DataAccess/Abstract/IContactDal.cs
@@ -0,0 +1,14 @@
+﻿using BlogProjectApp.DataAccess.Repositories;
+using BlogProjectApp.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProjectApp.DataAccess.Abstract
+{
+    public interface IContactDal : IRepositoryBase<Contact>
+    {
+    }
+}
---
+++ BlogProject/BlogProjectApp.DataAccess/Repositories/IRepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProjectApp.DataAccess.Repositories
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        T GetListEx(Expression<Func<T, bool>> filter);
+    }
+}
