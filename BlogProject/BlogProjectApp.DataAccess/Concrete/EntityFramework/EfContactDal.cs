30b32ffb5a96065147ec9cfa1e1df52933276e43
Fırat Alçın <firatalcin04@gmail.com>
2022-12-06 23:52:07 +0300

    EfCore Repository imp.

---
+++ BlogProject/BlogProjectApp.DataAccess/Concrete/EntityFramework/EfContactDal.cs
@@ -0,0 +1,10 @@
+﻿using BlogProjectApp.DataAccess.Abstract;
+using BlogProjectApp.DataAccess.Repositories;
+using BlogProjectApp.Entities.Concrete;
+
+namespace BlogProjectApp.DataAccess.Concrete.EntityFramework
+{
+    public class EfContactDal : RepositoryBase<Contact>, IContactDal
+    {
+    }
+}
---
+++ BlogProject/BlogProjectApp.DataAccess/Repositories/RepositoryBase.cs
@@ -0,0 +1,64 @@
+﻿using BlogProjectApp.DataAccess.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProjectApp.DataAccess.Repositories
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        public void Add(T entity)
+        {
+            using (ProjectDbContext db = new ProjectDbContext())
+            {
+                db.Add(entity);
+                db.SaveChanges();
+            }
+        }
+
+        public void Delete(T entity)
+        {
+            using (ProjectDbContext db = new ProjectDbContext())
+            {
+                db.Remove(entity);
+                db.SaveChanges();
+            }
+        }
+
+        public List<T> GetAll()
+        {
+            using (ProjectDbContext db = new ProjectDbContext())
+            {
+                return db.Set<T>().ToList();
+            }
+        }
+
+        public T GetById(int id)
+        {
+            using (ProjectDbContext db = new ProjectDbContext())
+            {
+                return db.Set<T>().Find(id);
+            }
+        }
+
+        public T GetListEx(Expression<Func<T, bool>> filter)
+        {
+            using (ProjectDbContext db = new ProjectDbContext())
+            {
+                return db.Set<T>().Where(filter).FirstOrDefault();
+            }
+        }
+
+        public void Update(T entity)
+        {
+            using (ProjectDbContext db = new ProjectDbContext())
+            {
+                db.Update(entity);
+                db.SaveChanges();
+            }
+        }
+    }
+}
