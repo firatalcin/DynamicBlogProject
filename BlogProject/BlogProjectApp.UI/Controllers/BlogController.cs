137b31eba701344058cf9dbdfd728a30e6a93f2c
Fırat Alçın <firatalcin04@gmail.com>
2022-12-17 02:22:36 +0300

    Kategori ve Blog Index sayfaları yapıldı

---
+++ BlogProject/BlogProjectApp.UI/Controllers/BlogController.cs
@@ -0,0 +1,28 @@
+﻿using BlogProjectApp.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProjectApp.UI.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IBlogService _blogService;
+
+        public BlogController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var list = _blogService.GetBlogListWithCategory();
+            return View(list);
+        }
+
+        public IActionResult BlogDetail(int id)
+        {
+
+            return View();
+        }
+    }
+}
