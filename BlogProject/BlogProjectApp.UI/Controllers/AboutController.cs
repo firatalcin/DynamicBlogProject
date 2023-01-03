821aec7dafcb6f2f8251c181e3a6fbab4dc6865d
Fırat Alçın <firatalcin04@gmail.com>
2023-01-03 16:43:10 +0300

    About İşlemleri

---
+++ BlogProject/BlogProjectApp.UI/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using BlogProjectApp.Business.Abstract;
+using BlogProjectApp.Business.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProjectApp.UI.Controllers
+{
+	public class AboutController : Controller
+	{
+		private readonly IAboutService _aboutService;
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public PartialViewResult SocialMediaAboutPartial()
+		{
+			var values = _aboutService.GetList();
+			return PartialView(values);
+		}
+	}
+}
